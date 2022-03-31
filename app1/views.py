436d8bfaa16e6c52e492171185f5f2dcc27c1eb8
leo173701 <leibo2014@yeah.net>
2022-03-30 23:24:01 -0400

    test1

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,15 @@
+# Create your views here.
+# import Http Response from django
 from django.shortcuts import render
 
-# Create your views here.
+
+# create a function
+def geeks_view(request):
+    # create a dictionary to pass
+    # data to the template
+    context = {
+        "data": "Gfg is the best",
+        "list": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+    # return response with template and context
+    return render(request, "geeks.html", context)
